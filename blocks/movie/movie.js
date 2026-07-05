4e7303e5eeefaaf9529f770a5da4023f9bbcbe81
Himansu Kumar Nayak <you@example.com>
2026-07-05 18:26:53 +0530

    Added new page for every movie

--- blocks/movie/movie.js
+++ blocks/movie/movie.js
@@ -11,7 +11,7 @@ export default async function decorate (block) {
 }
 
 async function fetchData(id, block) {
- let api_url = await fetch(`https://api.themoviedb.org/3/discover/movie/${id}?api_key=0a5247d162a6d3455cfaff2ae266f450&original_language=hi&region=IN`)
+ let api_url = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0a5247d162a6d3455cfaff2ae266f450&original_language=hi&region=IN`)
  let movie_url = await api_url.json()
  let result_movie = movie_url.results
  for(let i = 0; i < result_movie.length; i++) {
@@ -19,7 +19,7 @@ async function fetchData(id, block) {
   let movie_id = movie_item.id 
   let movie_title = movie_item.title
   let movie_img = movie_item.backdrop_path
-  let movie_name = document.createElement('p')
+  let movie_name = document.createElement('h2')
   let movie_thumbnail = document.createElement('img')
   movie_name.textContent = movie_title
   const card = document.createElement('div')
@@ -30,20 +30,7 @@ async function fetchData(id, block) {
     let optimized_img = createOptimizedPicture(full_movie_img, movie_title)
     card.append(optimized_img)
   }
-   /* Create a route for each movie */
-  function createSlug(title) {
-    return title
-    .toLowerCase()
-    .replace(/[^\w\s-]/g, '')
-    .trim()
-    .replace(/\s+/g, '-')
-  }
-  let slug = createSlug(movie_title)
-  const link = document.createElement('a')
-  link.href = `/movie?title=${slug}&id=${movie_id}`;
-  link.append(card)
-  card.append(movie_name)
-  block.append(link)
+  block.append(movie_name)
  }  
 }
 
--- blocks/movies/movies.js
+++ blocks/movies/movies.js
@@ -5,7 +5,7 @@ export default async function decorate (block) {
 }
 
 async function fetchData(block) {
- let api_url = await fetch(`https://api.themoviedb.org/3/movie?api_key=0a5247d162a6d3455cfaff2ae266f450&original_language=hi&region=IN`)
+ let api_url = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0a5247d162a6d3455cfaff2ae266f450&original_language=hi&region=IN`)
  let movie_url = await api_url.json()
  let result_movie = movie_url.results
  for(let i = 0; i < result_movie.length; i++) {
@@ -13,7 +13,7 @@ async function fetchData(block) {
   let movie_id = movie_item.id 
   let movie_title = movie_item.title
   let movie_img = movie_item.backdrop_path
-  let movie_name = document.createElement('h2')
+  let movie_name = document.createElement('p')
   let movie_thumbnail = document.createElement('img')
   movie_name.textContent = movie_title
   const card = document.createElement('div')
@@ -24,7 +24,20 @@ async function fetchData(block) {
     let optimized_img = createOptimizedPicture(full_movie_img, movie_title)
     card.append(optimized_img)
   }
+   /* Create a route for each movie */
+  function createSlug(title) {
+    return title
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, '')
+    .trim()
+    .replace(/\s+/g, '-')
+  }
+  let slug = createSlug(movie_title)
+  const link = document.createElement('a')
+  link.href = `/movie?title=${slug}&id=${movie_id}`;
+  link.append(card)
   card.append(movie_name)
+  block.append(link)
  }  
 }
 
