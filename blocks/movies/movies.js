ad90c350056b9d2dfaa361d36cc48009b5f9db35
Himansu Kumar Nayak <you@example.com>
2026-07-05 11:05:04 +0530

    Added images

--- blocks/movies/movies.js
+++ blocks/movies/movies.js
@@ -4,7 +4,7 @@ export default async function decorate (block) {
 }
 
 async function fetchData(block) {
- let api_url = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0a5247d162a6d3455cfaff2ae266f450&original_languag=hi&region=IN`)
+ let api_url = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0a5247d162a6d3455cfaff2ae266f450&original_language=hi&region=IN`)
  let movie_url = await api_url.json()
  let result_movie = movie_url.results
  for(let i = 0; i < result_movie.length; i++) {
@@ -15,6 +15,10 @@ async function fetchData(block) {
   let movie_name = document.createElement('p')
   let movie_thumbnail = document.createElement('img')
   movie_name.textContent = movie_title
+  if (movie_img) {
+    let full_movie_img = `https://image.tmdb.org/t/p/w500/${movie_img}`
+    let optimized_movie_img = createOptimizedPicture(full_movie_img, movie_title)
+  }
   movie_img.src = movie_thumbnail
   block.append(movie_name)
   block.append(movie_thumbnail)
