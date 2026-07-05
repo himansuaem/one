b59461980cbfc4352131c7d64fe156a310b29515
Himansu Kumar Nayak <you@example.com>
2026-07-05 11:45:10 +0530

    Added images

--- blocks/movies/movies.js
+++ blocks/movies/movies.js
@@ -17,7 +17,8 @@ async function fetchData(block) {
   movie_name.textContent = movie_title
   if (movie_img) {
     let full_movie_img = `https://image.tmdb.org/t/p/w500${movie_img}`
-      movie_thumbnail.src = movie_img
+    movie_thumbnail.src = full_movie_img
+    let optimized_img = createOptimizedPicture(full_movie_img, movie_title)
   }
   block.append(movie_name)
   block.append(movie_thumbnail)
