cef845dd60dfe448cc15fe80a7047009bbc0bdbc
Himansu Kumar Nayak <you@example.com>
2026-07-05 09:25:53 +0530

    Updated movies block

--- blocks/movies/movies.js
+++ blocks/movies/movies.js
@@ -7,7 +7,7 @@ async function fetchData(block) {
  let api_url = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0a5247d162a6d3455cfaff2ae266f450&original_languag=hi&region=IN`)
  let movie_url = await api_url.json()
  let result_movie = movie_url.results
- for( i = 0; i < result_movie.length; i++) {
+ for(let i = 0; i < result_movie.length; i++) {
   let movie_item = result_movie[i]
   let movie_id = movie_item.id 
   let movie_title = movie_item.title
